631c715cab3f9322d053b0f0a6fc2c8b7ca6f14b
John Maroli <john.m.maroli@nasa.gov>
2026-01-29 11:03:55 -0500

    Moved analysis plotting and removed unnecessary redundancy that copilot added.

--- lib/analyze_model.py
+++ lib/analyze_model.py
@@ -410,6 +410,57 @@ def analyze_model(analysis_parameters, model_dictionary, input_data, output_data
                                 print("Warning: Fit could not be estimated for " + f["txt"] + ",")
                                 print("         " + str(e))
                                 print("")
+                                
+                    # Plot 2D and 3D data with fitted function for visual inspection.
+                    if (save_visual == True or visual == True):
+                        if arg_count == 1:
+                            plt.figure()
+                            # Plot response data
+                            plt.scatter(x_data_fit[0], y_data_fit, marker='.', label='Response data')
+                            # Plot the fitted function
+                            x_sorted_indices = np.argsort(x_data_fit[0])
+                            x_sorted = x_data_fit[0][x_sorted_indices]
+                            y_fit_2d = product_function["function"]["fcn"](x_data_fit, *product_function["parameters"])
+                            y_fit_sorted = y_fit_2d[x_sorted_indices]
+                            plt.plot(x_sorted, y_fit_sorted, 'r-', linewidth=2, label='Fitted function')
+                            
+                            plt.title(product_function["template_string"])
+                            plt.xlabel(f_list[0])
+                            plt.legend()
+                            
+                            if save_visual == True:
+                                plt.savefig('./output/analysis_{}/{}.pdf'.format(analysis_dir_count, \
+                                            product_function["template_string"]))
+                                pltDict = {"x": x_data_fit[0].tolist(),
+                                       "y": y_data_fit.tolist()}
+                                pltDict["y_fit"] = y_fit_2d.tolist()
+                                mat4py.savemat('./output/analysis_{}/{}.mat'.format(analysis_dir_count, \
+                                               product_function["template_string"]), pltDict)
+                            if visual == True: plt.show()
+                        if arg_count == 2:
+                            plt.figure()
+                            # Plot the response data
+                            ax = plt.axes(projection='3d')
+                            ax.scatter3D(x_data_fit[0], x_data_fit[1], y_data_fit, c=y_data_fit, marker='o', label='Response data')
+                            # Plot the fitted function surface
+                            y_fit_3d = product_function["function"]["fcn"](x_data_fit, *product_function["parameters"])
+                            ax.plot_trisurf(x_data_fit[0], x_data_fit[1], y_fit_3d, alpha=0.5, color='red', label='Fitted function')
+                            
+                            ax.set_title(product_function["template_string"])
+                            ax.set_xlabel(f_list[0])
+                            ax.set_ylabel(f_list[1])
+                            ax.legend()
+                            
+                            if save_visual == True:
+                                plt.savefig('./output/analysis_{}/{}.pdf'.format(analysis_dir_count, \
+                                            product_function["template_string"]))
+                                pltDict = {"x": x_data_fit[0].tolist(),
+                                       "y": x_data_fit[1].tolist(),
+                                       "z": y_data_fit.tolist()}
+                                pltDict["z_fit"] = y_fit_3d.tolist()
+                                mat4py.savemat('./output/analysis_{}/{}.mat'.format(analysis_dir_count, \
+                                               product_function["template_string"]), pltDict)
+                            if visual == True: plt.show()
                 else:
                     # Handle constant bias at the zero point.
                     channel_bias = bias[0, channel_id].detach().numpy()
@@ -432,57 +483,6 @@ def analyze_model(analysis_parameters, model_dictionary, input_data, output_data
                     if verbose:
                         print("Constant " + channel_bias_str)
                         print()
-                
-                # Plot 2D and 3D data with fitted function for visual inspection.
-                if len(arg_list) > 0 and (save_visual == True or visual == True):
-                    if arg_count == 1:
-                        plt.figure()
-                        plt.scatter(x_data_fit[0], y_data_fit, marker='.', label='Data')
-                        # Plot the fitted function
-                        y_fit_1d = None
-                        if product_function["function"] is not None and len(product_function["parameters"]) > 0:
-                            x_sorted_indices = np.argsort(x_data_fit[0])
-                            x_sorted = x_data_fit[0][x_sorted_indices]
-                            y_fit_1d = product_function["function"]["fcn"](x_data_fit, *product_function["parameters"])
-                            y_fit_sorted = y_fit_1d[x_sorted_indices]
-                            plt.plot(x_sorted, y_fit_sorted, 'r-', linewidth=2, label='Fitted function')
-                        plt.title(product_function["template_string"])
-                        plt.xlabel(f_list[0])
-                        plt.legend()
-                        if save_visual == True:
-                            plt.savefig('./output/analysis_{}/{}.pdf'.format(analysis_dir_count, \
-                                        product_function["template_string"]))
-                            pltDict = {"x": x_data_fit[0].tolist(),
-                                   "y": y_data_fit.tolist()}
-                            if y_fit_1d is not None:
-                                pltDict["y_fit"] = y_fit_1d.tolist()
-                            mat4py.savemat('./output/analysis_{}/{}.mat'.format(analysis_dir_count, \
-                                           product_function["template_string"]), pltDict)
-                        if visual == True: plt.show()
-                    if arg_count == 2:
-                        plt.figure()
-                        ax = plt.axes(projection='3d')
-                        ax.scatter3D(x_data_fit[0], x_data_fit[1], y_data_fit, c=y_data_fit, marker='o', label='Data')
-                        # Plot the fitted function surface
-                        y_fit_2d = None
-                        if product_function["function"] is not None and len(product_function["parameters"]) > 0:
-                            y_fit_2d = product_function["function"]["fcn"](x_data_fit, *product_function["parameters"])
-                            ax.plot_trisurf(x_data_fit[0], x_data_fit[1], y_fit_2d, alpha=0.5, color='red', label='Fitted function')
-                        ax.set_title(product_function["template_string"])
-                        ax.set_xlabel(f_list[0])
-                        ax.set_ylabel(f_list[1])
-                        ax.legend()
-                        if save_visual == True:
-                            plt.savefig('./output/analysis_{}/{}.pdf'.format(analysis_dir_count, \
-                                        product_function["template_string"]))
-                            pltDict = {"x": x_data_fit[0].tolist(),
-                                   "y": x_data_fit[1].tolist(),
-                                   "z": y_data_fit.tolist()}
-                            if y_fit_2d is not None:
-                                pltDict["z_fit"] = y_fit_2d.tolist()
-                            mat4py.savemat('./output/analysis_{}/{}.mat'.format(analysis_dir_count, \
-                                           product_function["template_string"]), pltDict)
-                        if visual == True: plt.show()
                     
                 # Check if the candidate product function improves the accuracy of the model.
                 if sig_index > 0:
